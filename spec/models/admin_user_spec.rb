6124bce5567a2c3db2eadeb2a3cb6a01d66d0403
Ben Stein <bumstain@gmail.com>
2017-03-20 14:44:11 +0000

    Added Coveralls and cleaned up specs and superfluous files

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:admin_user) { create :admin_user }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+
+  it 'creates user with valid input' do 
+    expect { create :admin_user }.to change { AdminUser.count }.by(1)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,26 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
 
-  it 'creates user with valide input' do 
+  it 'creates user with valid input' do 
     expect { create :user }.to change { User.count }.by(1)
+  end
+
+  it 'does not create user when password mismatches confirmation' do
+    user = build :user, password_confirmation: 'mismatch'
+
+    expect(user).to_not be_valid
+    expect(user.errors.first).to match(
+      [:password_confirmation, "doesn't match Password"]
+    )
+  end
+
+  it 'has verified email' do
+    user = create :user
+    expect(user).to be_email_verified
+  end
+
+  it 'has unverified email if email is temporary' do
+    user = create :user, email: 'change@me'
+    expect(user).to_not be_email_verified
   end
 end
